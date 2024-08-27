68c3ac2595502d1cf7a168015278f1e7a4681356
김민 <128817770+rlaals0203@users.noreply.github.com>
2024-08-27 19:09:32 +0900

    Title: 합을 옆으로 밀어 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.cpp
+++ 240827/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.cpp
@@ -8,21 +8,25 @@ int main()
 	int n1 = 0;
 	int n2 = 0;
 	cin >> n1;
-	string* arr = new string[n1];
 
 	for (int i = 0; i < n1; i++)
 	{
 		int temp;
 		cin >> temp;
 		n2 += temp;
+	
 	}
 
-	for (int i = 0; i < n1; i++)
+	int n3 = to_string(n2).size();
+
+	string* arr = new string[n3];
+
+	for (int i = 0; i < n3; i++)
 	{
 		arr[i] = to_string(n2)[i];
 	}
 
-	for (int i = 1; i < n1 - 1; i++)
+	for (int i = 1; i < n3; i++)
 	{
 		cout << arr[i];
 	}
